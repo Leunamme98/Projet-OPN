1c27e883ff7f77f3839d3bc8a3d653ae7690029c
Leunamme98 <apedokossiemmanuel@gmail.com>
2024-09-19 02:56:35 +0100

    Création des Répository

---
+++ src/main/java/com/officePharmaceutiqueNationale/OPN/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.officePharmaceutiqueNationale.OPN.repository;
+
+import com.officePharmaceutiqueNationale.OPN.model.AgentOPN;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgentOpnRepository extends JpaRepository<AgentOPN, String> {
+}
